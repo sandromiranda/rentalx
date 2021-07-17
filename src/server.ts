48a6766b6a142598f1e1ee0b3f099523eef7efcb
Sandro Miranda <sandro.miranda@protonmail.ch>
2021-07-16 22:56:38 -0300

    adding debug in vscode

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,11 @@ const app = express();
 
 app.get("/", (request, response) => {
     return response.json({ message: "Hello World!"});
-})
+});
+
+app.post("/courses", (request, response) => {
+    const { name } = request.body;
+    return response.json({ name });
+});
 
 app.listen(3333, () => console.log("Server is running!"));
