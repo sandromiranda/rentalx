31ea07af90e00e89dd46d7fc935cbdb682ec1d3a
Sandro Miranda <sandro.miranda@protonmail.ch>
2021-07-18 19:21:10 -0300

    added singleton pattern to list the categories

--- src/modules/cars/repositories/CategoriesRepository.ts
+++ src/modules/cars/repositories/CategoriesRepository.ts
@@ -6,8 +6,17 @@ class CategoriesRepository implements ICategoriesRepository {
 
     private categories: Category[] = [];
 
-    constructor() {
+    private static INSTANCE: CategoriesRepository;
+
+    private constructor() {
         this.categories = [];
+    }
+
+    public static getInstance(): CategoriesRepository {
+        if(!CategoriesRepository.INSTANCE){
+            CategoriesRepository.INSTANCE = new CategoriesRepository();
+        }
+        return CategoriesRepository.INSTANCE;
     }
 
     create({ description, name }: ICreateCategoryDTO): void {
