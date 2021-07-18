0a7288f8791494f4dc8e0c592a05d0945a33dc01
Sandro Miranda <sandro.miranda@protonmail.ch>
2021-07-18 11:59:43 -0300

    added specifications repository and services

---
+++ src/modules/cars/repositories/ISpecificationsRepository.ts
@@ -0,0 +1,15 @@
+import { ICreateCategoryDTO } from "./ICategoriesRepository";
+import { Specification } from "../model/Specification";
+
+interface ICreateSpecificationDTO {
+    name: string;
+    description: string;
+}
+
+interface ISpecificationsRepository {
+
+    create({ description, name }: ICreateSpecificationDTO): void;
+    findByName(name: string): Specification;
+}
+
+export { ISpecificationsRepository, ICreateSpecificationDTO };
