2817de92ca31ecf46c9fe82cc9c5aedac0c6d2c8
Sandro Miranda <sandro.miranda@protonmail.ch>
2021-07-19 22:55:37 -0300

    added usecases for categories import

---
+++ src/modules/cars/useCases/importCategory/ImportCategoryController.ts
@@ -0,0 +1,16 @@
+import { Response, Request } from "express";
+import { ImportCategoryUseCase } from "./ImportCategoryUseCase";
+
+
+class ImportCategoryController {
+    constructor(private importCategorUseCase: ImportCategoryUseCase) {};
+    handle(request: Request, response: Response): Response {
+        const { file } = request;
+        
+        this.importCategorUseCase.execute(file);
+
+        return response.send();
+    }
+}
+
+export { ImportCategoryController };
