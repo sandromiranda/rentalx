18db3ccf44dd0eddaaf062940fcc997e4d29a930
Sandro Miranda <sandro.miranda@protonmail.ch>
2021-08-08 19:16:24 -0300

    added user token with JWT

---
+++ src/modules/accounts/UseCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,57 @@
+import { compare } from "bcrypt";
+import { sign } from "jsonwebtoken";
+import { inject, injectable } from "tsyringe";
+
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+    email: string;
+    password: string;
+}
+
+interface IResponse {
+    user: {
+        name: string;
+        email: string;
+    },
+    token: string;
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+
+    constructor(
+        @inject("UsersRepository")
+        private usersRepository: IUsersRepository
+    ) {}
+    async execute({ email, password }: IRequest) {
+        const user = await this.usersRepository.findByEmail(email);
+
+        if (!user){
+            throw new Error("Email or password incorrect");
+        }
+
+        const passwordMatch = await compare(password, user.password);
+
+        if (!passwordMatch) {
+            throw new Error("Email or password incorrect");
+        }
+
+        const token = sign({}, "51e90ab439ea8c875ac4e0f969e72d17", {
+            subject: user.id,
+            expiresIn: "1d"
+        }); //sandroignite
+
+        const tokenReturn: IResponse = {
+            token,
+            user: {
+                name: user.name,
+                email: user.email,
+            }
+        }
+
+        return tokenReturn;
+    }
+}
+
+export { AuthenticateUserUseCase };
