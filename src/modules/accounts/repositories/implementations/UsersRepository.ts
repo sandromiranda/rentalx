a4724c9ebbaf76d8368c1616a622dd602cc9903b
Sandro Miranda <sandro.miranda@protonmail.ch>
2021-08-08 10:15:10 -0300

    added if the user already exists

--- src/modules/accounts/repositories/implementations/UsersRepository.ts
+++ src/modules/accounts/repositories/implementations/UsersRepository.ts
@@ -12,16 +12,20 @@ class UsersRepository implements IUsersRepository {
         this.repository = getRepository(User);  
     }
 
-    async create({ name, username, email, driver_license, password }: ICreateUserDTO): Promise<void> {
+    async create({ name, email, driver_license, password }: ICreateUserDTO): Promise<void> {
         const user = this.repository.create({
             name,
-            username,
             email,
             driver_license,
             password
         });
 
         await this.repository.save(user);
+    }
+
+    async findByEmail(email: string): Promise<User> {
+        const user = await this.repository.findOne({email});
+        return user;
     }
 }
 
