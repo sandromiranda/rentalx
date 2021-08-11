95bdbf875cb8f3e1318b6a9f6691b2527581f7d2
Sandro Miranda <sandro.miranda@protonmail.ch>
2021-08-10 22:48:21 -0300

    adding avatar for the users

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,12 +1,15 @@
 import { Router } from "express";
 
 import { CreateUserController } from "../modules/accounts/UseCases/createUser/CreateUserController";
+import { UpdateUserAvatarController } from "../modules/accounts/UseCases/updateUserAvatar/UpdateUserAvatarController";
 
 const usersRoutes = Router();
 
 const createUserController = new CreateUserController();
+const updateUserAvatarController = new UpdateUserAvatarController
 
 usersRoutes.post("/", createUserController.handle);
 
+usersRoutes.patch("/avatar", updateUserAvatarController.handle);
 
 export { usersRoutes };
