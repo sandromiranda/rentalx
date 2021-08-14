38d0c24928ff7bbfe8def8ba4974d0c4f50d46a9
Sandro Miranda <sandro.miranda@protonmail.ch>
2021-08-14 16:44:28 -0300

    made sure the old avatars are being deleted when adding a new one

--- src/modules/accounts/UseCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
+++ src/modules/accounts/UseCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -1,5 +1,7 @@
 import { inject, injectable } from "tsyringe";
 
+import { deleteFile } from "../../../../utils/file";
+
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 
@@ -14,9 +16,13 @@ class UpdateUserAvatarUseCase {
         @inject("UsersRepository")
         private usersRepository: IUsersRepository
     ) {}
+
     async execute({ user_id, avatar_file }: IRequest) {
         const user = await this.usersRepository.findById(user_id);
 
+        if (user.avatar) {
+            await deleteFile(`./tmp/avatar/${user.avatar}`);
+        }
         user.avatar = avatar_file;
 
         await this.usersRepository.create(user);
