c7f98632acead53462b3b0c82ed5dfd003322fff
Sandro Miranda <sandro.miranda@protonmail.ch>
2021-08-26 23:53:20 -0300

    continue to work on cars

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # Cadastro de carro
 **RF** 
 Deve ser possível cadastrar um novo carro.
-Deve ser possível listar todas as categorias.
 **RN**
 Não deve ser possível cadastrar um carro com uma placa já existente.
 Não deve ser possível alterar a placa de um carro já cadastrado.
---
+++ src/modules/cars/infra/typeorm/entities/Car.ts
@@ -0,0 +1,16 @@
+
+
+class Car {
+    id: string;
+    name: string;
+    description: string;
+    daily_rate: number;
+    available: boolean;
+    license_plate: string;
+    fine_amount: number;
+    brand: string;
+    created_at: Date;
+    category_id: string;
+}
+
+export { Car }
