10035ac1d08f541e9df0669af940e96b560de4b4
Sandro Miranda <sandro.miranda@protonmail.ch>
2021-08-28 10:28:40 -0300

    added jest test for duplicated license plate attempt

--- src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
+++ src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
@@ -1,4 +1,5 @@
 import { CarsRepositoryInMemory } from "@modules/cars/repositories/in-memory/CarsRepositoryInMemory";
+import { AppError } from "@shared/errors/AppError";
 
 import { CreateCarUseCase } from "./CreateCarUseCase"
 
@@ -24,4 +25,31 @@ describe("Create Car", () => {
             category_id: "category",
         })
     });
+
+    it("should not be able to create a car with an existing license plate", async () => {
+
+        expect(async () => {
+            await createCarUseCase.execute({
+                name: "Car1",
+                description: "Description car",
+                daily_rate: 100,
+                license_plate: "ABC-1234",
+                fine_amount: 60,
+                brand: "Brand",
+                category_id: "category",
+            });
+
+            await createCarUseCase.execute({
+                name: "Car2",
+                description: "Description car",
+                daily_rate: 100,
+                license_plate: "ABC-1234",
+                fine_amount: 60,
+                brand: "Brand",
+                category_id: "category",
+            });
+        }).rejects.toBeInstanceOf(AppError);
+
+    });
+
 });
