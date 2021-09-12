f07c8b256e9c8bfb909d24e6492c8406f41f370c
Sandro Miranda <sandro.miranda@protonmail.ch>
2021-09-12 20:36:11 -0300

    updated to confirm that steps in readme is working

--- src/shared/infra/typeorm/migrations/1628363809355-AlterUserDeleteUsername.ts
+++ src/shared/infra/typeorm/migrations/1628363809355-AlterUserDeleteUsername.ts
@@ -3,7 +3,7 @@ import { MigrationInterface, QueryRunner, TableColumn } from "typeorm";
 export class AlterUserDeleteUsername1628363809355 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.dropTable("Users"); 
+        //await queryRunner.dropTable("Users"); 
         await queryRunner.dropColumn("users", "username");
         await queryRunner.renameColumn("users", "admin", "isAdmin");
 
