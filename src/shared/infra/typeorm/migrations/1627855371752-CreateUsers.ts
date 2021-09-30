15470b782944ac197c05cd03a27716c7ee28eed4
Sandro Miranda <sandro.miranda@protonmail.ch>
2021-09-30 08:31:46 -0300

    created migrations for rentals

--- src/shared/infra/typeorm/migrations/1627855371752-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1627855371752-CreateUsers.ts
@@ -9,7 +9,8 @@ export class CreateUsers1627855371752 implements MigrationInterface {
                 columns: [
                     {
                         name: "id",
-                        type: "uuid"
+                        type: "uuid",
+                        isPrimary: true
                     },
                     {
                         name: "name",
