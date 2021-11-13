507891513b468bb54aadd6030385a49a7da1cd43
Sandro Miranda <sandro.miranda@protonmail.ch>
2021-11-13 14:52:22 -0300

    test it should not allow to create a rental when there is one already for the car or the user

--- src/modules/rentals/useCases/CreateRentalUseCase.spec.ts
+++ src/modules/rentals/useCases/CreateRentalUseCase.spec.ts
@@ -1,3 +1,4 @@
+import { AppError } from "@shared/errors/AppError";
 import { RentalsRepositoryInMemory } from "../repositories/in-memory/RentalsRepositoryInMemory"
 import { CreateRentalUseCase } from "./CreateRentalUseCase"
 
@@ -22,7 +23,43 @@ describe("Create Rental", () => {
 
         expect(rental).toHaveProperty("id")
         expect(rental).toHaveProperty("start_date")
+    })
+
+    it("Should not be able to create a new rental if there is an existing one for the user", async () => {
+        
+        expect(async () => {
+            await createRentalUseCase.execute({
+                user_id: "12345",
+                car_id: "121212",
+                expected_return_date: new Date(),     
+            })
+
+            await createRentalUseCase.execute({
+                user_id: "12345",
+                car_id: "121212",
+                expected_return_date: new Date(),     
+            })
+        }).rejects.toBeInstanceOf(AppError)
 
     })
+
+    it("Should not be able to create a new rental if there is an existing one for the car", async () => {
+        
+        expect(async () => {
+            await createRentalUseCase.execute({
+                user_id: "1234",
+                car_id: "test",
+                expected_return_date: new Date(),     
+            })
+
+            await createRentalUseCase.execute({
+                user_id: "4321",
+                car_id: "test",
+                expected_return_date: new Date(),     
+            })
+        }).rejects.toBeInstanceOf(AppError)
+
+    })
+
 
 })
