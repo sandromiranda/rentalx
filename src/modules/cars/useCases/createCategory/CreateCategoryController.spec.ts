24d94f3a03118e863d05c250adfaa26c665c38d6
Sandro Miranda <sandro.miranda@protonmail.ch>
2021-11-16 21:59:12 -0300

    configuring the environment for supertest to work

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
@@ -0,0 +1,15 @@
+import request from "supertest";
+
+import { app } from "@shared/infra/http/app";
+
+describe("Create category controller", async () => {
+    it("should be able to create a new category", async () => {
+        const response = await request(app).post("/categories").send({
+            name: "Category Supertest",
+            description: "Desc for category supertest"
+        });
+
+        expect(response.status).toBe(201);
+    });
+});
+
--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -7,6 +7,7 @@ export default async(host = "database"): Promise<Connection> => {
     return createConnection(
         Object.assign(defaultOptions, {
             host,
+            database: process.env.NODE_ENV === "test" ? "rentx_test" : defaultOptions.database,
         })
 
     );
