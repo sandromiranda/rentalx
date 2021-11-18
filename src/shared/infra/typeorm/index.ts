e8d41e2b3a1df754b5dfb59c406ed834906b7dc4
Sandro Miranda <sandro.miranda@protonmail.ch>
2021-11-17 22:58:49 -0300

    continuing integration test

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -6,8 +6,11 @@ export default async(host = "database"): Promise<Connection> => {
 
     return createConnection(
         Object.assign(defaultOptions, {
-            host,
-            database: process.env.NODE_ENV === "test" ? "rentx_test" : defaultOptions.database,
+            host: process.env.NODE_ENV === "test" ? "localhost" : host,
+            database: 
+                process.env.NODE_ENV === "test" 
+                    ? "rentx_test" 
+                    : defaultOptions.database,
         })
 
     );
