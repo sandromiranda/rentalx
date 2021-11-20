cb313a0acc480cbf1ead02d07bca94cb3aeb256b
Sandro Miranda <sandro.miranda@protonmail.ch>
2021-11-20 12:16:49 -0300

    finising use case for car devolution

--- src/shared/container/providers/DateProvider/implementations/DayjsDateProvider.ts
+++ src/shared/container/providers/DateProvider/implementations/DayjsDateProvider.ts
@@ -6,6 +6,7 @@ import { IDateProvider } from "../IDateProvider";
 dayjs.extend(utc);
 
 class DayjsDateProvider implements IDateProvider{
+    
 
     compareInHours(start_date: Date, end_date: Date): number {
         const end_date_utc = this.convertToUTC(end_date)
@@ -19,6 +20,12 @@ class DayjsDateProvider implements IDateProvider{
 
     dateNow(): Date {
         return dayjs().toDate();
+    }
+
+    compareInDays(start_date: Date, end_date: Date): number {
+        const end_date_utc = this.convertToUTC(end_date)
+        const start_date_utc = this.convertToUTC(start_date)
+        return dayjs(end_date_utc).diff(start_date_utc, "days")
     }
 
 }
