073c7a9930be03220df2d5eef1aedf9a12f3c0c9
Sandro Miranda <sandro.miranda@protonmail.ch>
2022-03-14 09:00:13 -0300

    worked on the html for email

--- src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
+++ src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
@@ -1,5 +1,7 @@
 import { injectable } from "tsyringe";
 import nodemailer, { Transporter } from "nodemailer";
+import handlebars from "handlebars";
+import fs from "fs";
 
 import { IMailProvider } from "../IMailProvider";
 
@@ -22,13 +24,23 @@ class EtherealMailProvider implements IMailProvider {
             this.client = transporter;
         }).catch((err) => console.log(err));
     }
-    async sendMail(to: string, subject: string, body: string): Promise<void> {
+    async sendMail(
+        to: string, 
+        subject: string, 
+        variables: any,
+        path: string    
+    ): Promise<void> {
+        const templateFileContent = fs.readFileSync(path).toString("utf-8");
+
+        const templateParse = handlebars.compile(templateFileContent);
+
+        const templateHTML = templateParse(variables);
+
         const message = await this.client.sendMail({
             to,
             from: "Rentx <noreply@rentx.com.br>",
             subject,
-            text: body,
-            html: body,
+            html: templateHTML,
         });
 
         console.log("Message send: %s", message.messageId);
