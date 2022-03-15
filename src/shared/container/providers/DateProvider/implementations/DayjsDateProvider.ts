6217a1a3859bb1de2c08a42f32bc754b4e445687
Sandro Miranda <sandro.miranda@protonmail.ch>
2022-03-15 07:39:42 -0300

    finished reset password process

--- src/shared/container/providers/DateProvider/implementations/DayjsDateProvider.ts
+++ src/shared/container/providers/DateProvider/implementations/DayjsDateProvider.ts
@@ -35,6 +35,10 @@ class DayjsDateProvider implements IDateProvider{
         return dayjs().add(hours, "hour").toDate()
     }
 
+    comparaIfBefore(start_date: Date, end_date: Date): boolean {
+        return dayjs(start_date).isBefore(end_date)
+    }
+
 }
 
 export { DayjsDateProvider }
